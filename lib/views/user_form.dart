9f53f25504572ddfff174506e601676091b9dd47
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-06-14 21:21:52 -0400

    finalizado validações iniciais de formulario, iniciando o map de dados de usuario

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+
 class UserForm extends StatelessWidget {
+  
+  final _form = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +14,18 @@ class UserForm extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.save),
             onPressed: (){
-              //retornar uma tela, no caso a HOME
-              Navigator.of(context).pop();
+              //chamando as validações, retorna um bool
+              //se retornar algum texto, isValid sera um erro
+              final isValid = _form.currentState!.validate();
+
+              //verdade se estiver tudo certo, não ja de imediato retorna o erro
+              if(isValid){
+                //pegando o formulario e salvando. salvando aonde??
+                _form.currentState!.save();
+
+                //retornar uma tela, no caso a HOME
+                Navigator.of(context).pop();
+              }
             },
           )
         ],
@@ -20,11 +33,29 @@ class UserForm extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(20),
         child: Form(
+            key: _form,
             child: Column(
               children: <Widget>[
                 TextFormField(
                   //vai ter texto padrão
                   decoration: InputDecoration(labelText: 'Nome'),
+
+                  //validação, chama de volta?
+                  validator: (value){
+                    if(value == null || value.trim().isEmpty)
+                      return 'Nome inválido';
+
+                    if(value.trim().length < 3)
+                      return 'Nome muito curto';
+
+                    //esta tudo certo
+                    return null;
+                  },
+                  
+                  //metodo salve sendo chamado
+                  onSaved: (value){
+                    print(value); //ele salva o valor inserido no input "nome"
+                  },
                 ),
 
                  TextFormField(
